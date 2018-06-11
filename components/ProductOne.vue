36c0782adeb56f36fd67ca3711940eb638885ca2
Franklin Antony <franklinanton.007@gmail.com>
2018-06-12 01:35:54 +0530

    Two Components Added

---
+++ components/ProductOne.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="product-one">
+     <h2>Product One</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ components/ProductTwo.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="product-two">
+     <h2>Product Two</h2>
+     <ul>
+        <li v-for=""></li>
+     </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style>
+</style>
